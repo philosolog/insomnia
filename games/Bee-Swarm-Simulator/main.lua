4b61442e5b50e6671deb9839dfa1742e3e5c019a
philosolog <philosolog@gmail.com>
2022-07-23 02:56:24 -0700

    e

--- games/Bee-Swarm-Simulator/main.lua
+++ games/Bee-Swarm-Simulator/main.lua
@@ -663,10 +663,10 @@ information:CreateLabel("⚠️ = unsafe to use")
 information:CreateLabel("⚙ = configurable (in settings)")
 information:CreateButton("discord server", function() setclipboard("https://discord.gg/aVgrSFCHpu") end)
 local gui_killer = 1_1_2:CreateButton("kill gui", function() 
-	Window:Destroy()
+	--Window:Destroy()
 end) 
 
-gui_killer:CreateKeybind("RightControl", function(Key)end)
+gui_killer:CreateKeybind("X", function(Key)end)
 
 local farmo = farmtab:CreateSection("farm")
 local fielddropdown = farmo:CreateDropdown("field", fieldstable, function(String) sleepy.vars.field = String end) fielddropdown:SetOption(fieldstable[8])
@@ -777,7 +777,7 @@ dispsettings:CreateToggle("Mountain Top Booster", nil,  function(State) sleepy.d
 dispsettings:CreateToggle("Blue Field Booster", nil,  function(State) sleepy.dispensesettings.blue = not sleepy.dispensesettings.blue end)
 dispsettings:CreateToggle("Red Field Booster", nil,  function(State) sleepy.dispensesettings.red = not sleepy.dispensesettings.red end)
 local guisettings = setttab:CreateSection("GUI Settings")
-local uitoggle = guisettings:CreateToggle("UI Toggle", nil, function(State) Window:Toggle(State) end) uitoggle:CreateKeybind(tostring(Config.Keybind):gsub("RightShift", ""), function(Key) Config.Keybind = Enum.KeyCode[Key] end) uitoggle:SetState(true) -- ?
+local uitoggle = guisettings:CreateToggle("UI Toggle", nil, function(State) Window:Toggle(State) end) uitoggle:CreateKeybind(tostring(Config.Keybind):gsub("Enum.KeyCode.", ""), function(Key) Config.Keybind = Enum.KeyCode[Key] end) uitoggle:SetState(true)
 guisettings:CreateColorpicker("UI Color", function(Color) Window:ChangeColor(Color) end)
 local themes = guisettings:CreateDropdown("Image", {"Default","Hearts","Abstract","Hexagon","Circles","Lace With Flowers","Floral"}, function(Name) if Name == "Default" then Window:SetBackground("2151741365") elseif Name == "Hearts" then Window:SetBackground("6073763717") elseif Name == "Abstract" then Window:SetBackground("6073743871") elseif Name == "Hexagon" then Window:SetBackground("6073628839") elseif Name == "Circles" then Window:SetBackground("6071579801") elseif Name == "Lace With Flowers" then Window:SetBackground("6071575925") elseif Name == "Floral" then Window:SetBackground("5553946656") end end)themes:SetOption("Default")
 local sleepys = setttab:CreateSection("Configs")
