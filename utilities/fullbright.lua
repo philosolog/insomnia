cc88146f05c032d6562c3766ec69ade4bfbf9993
philosolog <philosolog@gmail.com>
2022-07-25 06:18:09 -0700

    Co-authored-by: definedM <definedM@users.noreply.github.com>

--- utilities/fullbright.lua
+++ utilities/fullbright.lua
@@ -1,1 +1,21 @@
 -- TODO
+
+local lighting = game:GetService("Lighting")
+sleepy.lightingData = {}
+
+lighting:GetPropertyChangedSignal("ClockTime"):Connect(function()
+    lighting.ClockTime = 13
+end)
+lighting:GetPropertyChangedSignal("OutdoorAmbient"):Connect(function()
+    lighting.OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+end)
+lighting:GetPropertyChangedSignal("FogEnd"):Connect(function()
+    lighting.FogEnd = 10e6
+end)
+
+setmetatable(sleepy.toggles.fullbright, {
+    __newindex = function()
+        
+        return
+    end
+})
