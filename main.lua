f481b9e1513249f3bd24dc5efd6a70043baa67e5
philosolog <philosolog@gmail.com>
2022-07-27 03:51:33 -0700

    e

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 1537690962 then
-    loadstring(game:HttpGet(getgenv().sleepy.repository.."Bee-Swarm-Simulator/main.lua"))()
+    loadstring(game:HttpGet(getgenv().sleepy.repository.."/games/Bee-Swarm-Simulator/main.lua"))()
 end
 
 -- TODO: Create dynamic links for githubusercontent references.
