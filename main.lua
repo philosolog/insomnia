b6223e9915cc681b8f336ac2bd4fffb9fb3363bf
philosolog <philosolog@gmail.com>
2022-07-27 03:37:34 -0700

    e

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 getgenv().start_time = tick()
-getgenv().sleepy.repository = "https://raw.githubusercontent.com/philosolog/sleepy-pbe/main"
+getgenv().sleepy = {
+	repository = "https://raw.githubusercontent.com/philosolog/sleepy-pbe/main"
+}
 
 if game.PlaceId == 1537690962 then
     loadstring(game:HttpGet(getgenv().sleepy.repository.."/main.lua"))()
