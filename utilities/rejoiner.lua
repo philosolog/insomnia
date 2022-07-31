f0a4b9303e4bd3a90e6c1219f18539a8eef12359
philosolog <philosolog@gmail.com>
2022-07-30 22:24:36 -0700

    e

--- utilities/rejoiner.lua
+++ utilities/rejoiner.lua
@@ -1,1 +1,6 @@
-game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+local TeleportService = game:GetService("TeleportService")
+local Player = game:GetService("Players").LocalPlayer
+
+TeleportService:Teleport(game.PlaceId, Player)
+
+-- !: I'm keeping the references unsimplified so I can implement autorejoin for the same private server. (using external tools ig)
